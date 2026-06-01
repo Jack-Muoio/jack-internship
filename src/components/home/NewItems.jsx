03f99b4162579694834a02ae2f619666191bc78e
Jack-Muoio <jackmuoio81@gmail.com>
2026-06-01 14:18:20 -0400

    New Items Feature

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+
+const CountdownTimer = ({ expiryDate }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const calculateTimeLeft = () => {
+      const difference = expiryDate - Date.now();
+      setTimeLeft(difference > 0 ? difference : 0);
+    };
+    calculateTimeLeft();
+    const timer = setInterval(calculateTimeLeft, 1000);
+    return () => clearInterval(timer);
+  }, [expiryDate]);
+
+  const hours = Math.floor(timeLeft / (1000 * 60 * 60));
+  const minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+  const seconds = Math.floor((timeLeft / 1000) % 60);
+
+  return <div className="de_countdown">{timeLeft > 0 ? `${hours}h ${minutes}m ${seconds}s` : "EXPIRED"}</div>;
+};
 
 const NewItems = () => {
+  const [items, setItems] = useState([]);
+  const [isLeftHovered, setIsLeftHovered] = useState(false);
+  const [isRightHovered, setIsRightHovered] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const [sliderRef, instanceRef] = useKeenSlider({
+    loop: true,
+    slides: {
+      perView: 4,
+      spacing: 10,
+    },
+    breakpoints: {
+      "(max-width: 1000px)": { slides: { perView: 3, spacing: 12 } },
+      "(max-width: 768px)": { slides: { perView: 2, spacing: 10 } },
+      "(max-width: 480px)": { slides: { perView: 1, spacing: 10 } },
+    },
+  });
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems",
+        );
+        setItems(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching new items:", error);
+        setLoading(false);
+      }
+    };
+    fetchItems();
+  }, []);
+
+  useEffect(() => {
+    if (instanceRef.current) {
+      instanceRef.current.update();
+    }
+  }, [items, loading, instanceRef]);
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +75,166 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+          <div className="col-lg-12" style={{ position: "relative" }}>
+            <div ref={sliderRef} className="keen-slider">
+              {loading
+                ? new Array(4).fill(0).map((_, index) => (
+                    <div
+                      className="keen-slider__slide"
+                      key={index}
+                      style={{ overflow: "visible" }}
+                    >
+                      <div className="nft__item" style={{ width: "100%", margin: 0 }}>
+                        <div className="author_list_pp">
+                          <Link
+                            to="/author"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title="Creator: Monica Lucas"
+                          >
+                            <div className="skeleton-box" style={{ width: "50px", height: "50px", borderRadius: "50%" }}></div>
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <div className="de_countdown">
+                          <div className="skeleton-box" style={{ width: "60px", height: "15px" }}></div>
+                        </div>
+                        <div className="nft__item_wrap" style={{ height: "200px" }}>
+                          <div className="skeleton-box" style={{ width: "100%", height: "100%" }}></div>
+                        </div>
+                        <div className="nft__item_info">
+                          <h4>
+                            <div className="skeleton-box" style={{ width: "100px", height: "20px" }}></div>
+                          </h4>
+                          <div className="nft__item_price">
+                            <div className="skeleton-box" style={{ width: "50px", height: "15px" }}></div>
+                          </div>
+                          <div className="nft__item_like">
+                            <i className="fa fa-heart"></i>
+                            <div className="skeleton-box" style={{ width: "30px", height: "15px", display: "inline-block", marginLeft: "5px" }}></div>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  ))
+                : items.map((item, index) => (
+                    <div
+                      className="keen-slider__slide"
+                      key={item.id || index}
+                      style={{ overflow: "visible" }}
+                    >
+                      <div className="nft__item" style={{ width: "100%", margin: 0 }}>
+                        <div className="author_list_pp">
+                          <Link
+                            to="/author"
+                            data-bs-toggle="tooltip"
+                            data-bs-placement="top"
+                            title="Creator: Monica Lucas"
+                          >
+                            <img className="lazy" src={item.authorImage} alt="" />
+                            <i className="fa fa-check"></i>
+                          </Link>
+                        </div>
+                        <CountdownTimer expiryDate={item.expiryDate} />
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
+                        <div className="nft__item_wrap">
+                          <div className="nft__item_extra">
+                            <div className="nft__item_buttons">
+                              <button>Buy Now</button>
+                              <div className="nft__item_share">
+                                <h4>Share</h4>
+                                <a href="" target="_blank" rel="noreferrer">
+                                  <i className="fa fa-facebook fa-lg"></i>
+                                </a>
+                                <a href="" target="_blank" rel="noreferrer">
+                                  <i className="fa fa-twitter fa-lg"></i>
+                                </a>
+                                <a href="">
+                                  <i className="fa fa-envelope fa-lg"></i>
+                                </a>
+                              </div>
+                            </div>
+                          </div>
+
+                          <Link to="/item-details">
+                            <img
+                              src={item.nftImage}
+                              className="lazy nft__item_preview"
+                              alt=""
+                            />
+                          </Link>
+                        </div>
+                        <div className="nft__item_info">
+                          <Link to="/item-details">
+                            <h4>{item.title}</h4>
+                          </Link>
+                          <div className="nft__item_price">{item.price} ETH</div>
+                          <div className="nft__item_like">
+                            <i className="fa fa-heart"></i>
+                            <span>{item.likes}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
             </div>
-          ))}
+            <button
+              onMouseEnter={() => setIsLeftHovered(true)}
+              onMouseLeave={() => setIsLeftHovered(false)}
+              onClick={() => instanceRef.current?.prev()}
+              style={{
+                position: "absolute",
+                left: "14px",
+                top: "50%",
+                transform: `translate(-50%, -50%) ${isLeftHovered ? "scale(1.12)" : "scale(1)"}`,
+                transition: "all 0.25s ease-in-out",
+                zIndex: 10,
+                borderRadius: "50%",
+                width: "45px",
+                height: "45px",
+                border: isLeftHovered ? "1px solid #a0a0a0" : "1px solid #ccc",
+                backgroundColor: "#fff",
+                boxShadow: isLeftHovered
+                  ? "0px 6px 14px rgba(0, 0, 0, 0.3)"
+                  : "none",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                outline: "none",
+              }}
+            >
+              &#10094;
+            </button>
+            <button
+              onMouseEnter={() => setIsRightHovered(true)}
+              onMouseLeave={() => setIsRightHovered(false)}
+              onClick={() => instanceRef.current?.next()}
+              style={{
+                position: "absolute",
+                right: "14px",
+                top: "50%",
+                transform: `translate(50%, -50%) ${isRightHovered ? "scale(1.12)" : "scale(1)"}`,
+                transition: "all 0.25s ease-in-out",
+                zIndex: 10,
+                borderRadius: "50%",
+                width: "45px",
+                height: "45px",
+                border: isRightHovered ? "1px solid #a0a0a0" : "1px solid #ccc",
+                backgroundColor: "#fff",
+                boxShadow: isRightHovered
+                  ? "0px 6px 14px rgba(0, 0, 0, 0.3)"
+                  : "none",
+                cursor: "pointer",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                outline: "none",
+              }}
+            >
+              &#10095;
+            </button>
+          </div>
         </div>
       </div>
     </section>
