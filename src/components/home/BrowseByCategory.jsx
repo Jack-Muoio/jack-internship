57d3b932e0a56d930445a2d1c92fe44322df2213
Jack-Muoio <jackmuoio81@gmail.com>
2026-06-02 23:30:39 -0400

    Browse by Category animation update

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -15,43 +15,43 @@ const BrowseByCategory = () => {
     <section id="section-category" className="no-top">
       <div className="container">
         <div className="row">
-          <div className="col-lg-12" data-aos="fade" data-aos-delay="100" data-aos-duration="1000">
+          <div className="col-lg-12" data-aos="fade" data-aos-delay="150" data-aos-duration="1000">
             <div className="text-center">
               <h2>Browse by category</h2>
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="100" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="300" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="300" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="400" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="400" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="500" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="500" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="600" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="600" data-aos-duration="1000" data-aos-easing="ease">
+          <div className="col-md-2 col-sm-4 col-6 mb-sm-30" data-aos="subtle-fade-left" data-aos-delay="700" data-aos-duration="1000" data-aos-easing="ease">
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
