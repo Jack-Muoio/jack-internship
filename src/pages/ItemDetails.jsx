4048609c526cade96a63c24803391e4b2432579c
Jack-Muoio <jackmuoio81@gmail.com>
2026-06-02 20:30:15 -0400

    Made Item Details links and page dynamic

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import Skeleton from "../components/UI/Skeleton";
 
 const ItemDetails = () => {
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { nftId } = useParams();
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+
+    const fetchItemDetails = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/itemDetails?nftId=${nftId}`
+        );
+        setItem(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching item details:", error);
+        setLoading(false);
+      }
+    };
+    fetchItemDetails();
+  }, [nftId]);
 
   return (
     <div id="wrapper">
@@ -17,43 +35,59 @@ const ItemDetails = () => {
           <div className="container">
             <div className="row">
               <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
+                {loading ? (
+                  <Skeleton width="100%" height="100%" />
+                ) : (
+                  <img
+                    src={item?.nftImage}
+                    className="img-fluid img-rounded mb-sm-30 nft-image"
+                    alt=""
+                  />
+                )}
               </div>
               <div className="col-md-6">
                 <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
+                  {loading ? (
+                    <Skeleton width="300px" height="40px" />
+                  ) : (
+                    <h2>{item?.title}</h2>
+                  )}
 
                   <div className="item_info_counts">
                     <div className="item_info_views">
                       <i className="fa fa-eye"></i>
-                      100
+                      {loading ? <Skeleton width="40px" height="20px" /> : item?.views}
                     </div>
                     <div className="item_info_like">
                       <i className="fa fa-heart"></i>
-                      74
+                      {loading ? <Skeleton width="40px" height="20px" /> : item?.likes}
                     </div>
                   </div>
                   <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
+                    {loading ? (
+                      <Skeleton width="100%" height="80px" />
+                    ) : (
+                      item?.description
+                    )}
                   </p>
                   <div className="d-flex flex-row">
                     <div className="mr40">
                       <h6>Owner</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={loading ? "#" : `/author/${item?.ownerId}`}>
+                            {loading ? (
+                              <Skeleton width="50px" height="50px" borderRadius="50%" />
+                            ) : (
+                              <img className="lazy" src={item?.ownerImage} alt="" />
+                            )}
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={loading ? "#" : `/author/${item?.ownerId}`}>
+                            {loading ? <Skeleton width="100px" height="20px" /> : item?.ownerName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -64,13 +98,19 @@ const ItemDetails = () => {
                       <h6>Creator</h6>
                       <div className="item_author">
                         <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
+                          <Link to={loading ? "#" : `/author/${item?.creatorId}`}>
+                            {loading ? (
+                              <Skeleton width="50px" height="50px" borderRadius="50%" />
+                            ) : (
+                              <img className="lazy" src={item?.creatorImage} alt="" />
+                            )}
                             <i className="fa fa-check"></i>
                           </Link>
                         </div>
                         <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                          <Link to={loading ? "#" : `/author/${item?.creatorId}`}>
+                            {loading ? <Skeleton width="100px" height="20px" /> : item?.creatorName}
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -78,7 +118,9 @@ const ItemDetails = () => {
                     <h6>Price</h6>
                     <div className="nft-item-price">
                       <img src={EthImage} alt="" />
-                      <span>1.85</span>
+                      <span>
+                        {loading ? <Skeleton width="50px" height="20px" /> : item?.price}
+                      </span>
                     </div>
                   </div>
                 </div>
